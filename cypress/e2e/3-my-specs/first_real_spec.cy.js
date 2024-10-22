fb6228eded2182b108db9460c7f02154334c1611
Dylan Henry <dhenry@comptia.org>
2024-10-22 16:57:14 -0600

    Adding jsconfig.json and finishing the basic documentation from Cypress

--- cypress/e2e/3-my-specs/first_real_spec.cy.js
+++ cypress/e2e/3-my-specs/first_real_spec.cy.js
@@ -1,5 +1,17 @@
 describe('My First Real Test', () => {
-  it('Visits the Kitchen Sink', () => {
+  it('clicks the link "type"', () => {
     cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be a on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it
+    cy.get('.action-email').type('fake@email.com')
+
+    // Verify that the value has been updated
+    cy.get('.action-email').should('have.value', 'fake@email.com')
   })
 })
